9594e44026c7ada4c1f70afa4c16535be9f0c71e
NicJcoding <nijcoding@gmail.com>
2022-11-02 20:32:28 -0500

    Created a project to push myself to learn Navigation Control

---
+++ Bailey's Adventure/GodVC.swift
@@ -0,0 +1,29 @@
+//
+//  GodVC.swift
+//  Bailey's Adventure
+//
+//  Created by Nicolas Juarez on 11/2/22.
+//
+
+import UIKit
+
+class GodVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
